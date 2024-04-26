b68304f1182a7fd414cf9dfa69e1044b12507160
KuyLyhour24 <kuylyhour24programmer@gmail.com>
2024-04-27 04:10:55 +0700

    Add dependency

--- src/main/java/com/lyhour/java/study/phone_shop/repository/ModelRepository.java
+++ src/main/java/com/lyhour/java/study/phone_shop/repository/ModelRepository.java
@@ -1,10 +1,7 @@
 package com.lyhour.java.study.phone_shop.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.lyhour.java.study.phone_shop.entity.Brand;
 import com.lyhour.java.study.phone_shop.entity.Model;
 
 public interface ModelRepository extends JpaRepository<Model, Integer> {
